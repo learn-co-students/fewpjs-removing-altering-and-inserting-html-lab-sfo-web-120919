0d48899d954523fbbe84db9495e941ff6de38de0
Girija Viswanathan <58586934+GirijaViswa@users.noreply.github.com>
2020-01-27 14:26:10 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+let ele = document.getElementById('main');
+ele.remove();
+
+let element = document.createElement('h1')
+document.body.appendChild(element)
+element.id = "victory"
+
+let newHeader = document.querySelector('h1#victory')
+newHeader.innerHTML="YOUR-NAME is the champion</h1>";
+
+
